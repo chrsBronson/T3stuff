c923f949c3b86282c786c1c1e76f2f06c1036d6b
chrsBronson <yzharko@ya.ru>
2024-04-29 14:34:26 +0300

    very bad T3 example

--- cmd.cpp
+++ cmd.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
-
 int main()
 {
-  std::cout << "wazup goth programmer nation\n";
+  std::string cmd;
+  //contextCMD1
+  //contextCMD2
+  while (std::cin >> cmd)
+  {
+    if (cmd == "CMD1")
+      {
+        //input paramsCMD1
+        //do smth with contextCMD1 and paramsCMD1
+      }
+    else if (cmd == "CMD2")
+      {
+        //input paramsCMD2
+        //do smth with contextCMD2 and paramsCMD2
+      }
+    else
+      {
+        std::cerr << "<INVALID COMMAND>\n";
+      }
+  }
 }
