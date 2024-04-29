dfd635f2f9579b02b0ad1bd9e4095e18ea10e468
chrsBronson <yzharko@ya.ru>
2024-04-29 14:52:24 +0300

    dispathing cmds with map example

--- cmd.cpp
+++ cmd.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <map>
+#include <functional>
 namespace goth
 {
   void cmd1(std::istream &, std::ostream & out)
@@ -12,20 +14,19 @@ namespace goth
 }
 int main()
 {
+  std::map< std::string, std::function < void(std::istream &, std::ostream &) > > cmds;
+  cmds["CMD1"] = goth::cmd1;
+  cmds["CMD2"] = goth::cmd2;
   std::string cmd;
   while (std::cin >> cmd)
   {
-    if (cmd == "CMD1")
-      {
-        goth::cmd1(std::cin, std::cout);
-      }
-    else if (cmd == "CMD2")
-      {
-        goth::cmd2(std::cin, std::cout);
-      }
-    else
-      {
-        std::cerr << "<INVALID COMMAND>\n";
-      }
+    try
+    {
+      cmds.at(cmd)(std::cin, std::cout);
+    }
+    catch (const std::out_of_range &)
+    {
+      std::cerr << "<INVALID COMMAND>\n";
+    }
   }
 }
