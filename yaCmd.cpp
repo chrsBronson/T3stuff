49fc6b9a69618e582cc26f256cccd69fb72076e7
chrsBronson <yzharko@ya.ru>
2024-04-29 15:12:17 +0300

    ya solid example with one map

--- yaCmd.cpp
+++ yaCmd.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
-
+#include <map>
+#include <functional>
+namespace goth
+{
+  void cmd1(int &, std::istream &, std::ostream & out)
+  {
+    out << "CMD1 dispatched\n";
+  }
+  void cmd2(int &, std::istream &, std::ostream & out)
+  {
+    out << "CMD2 dispatched\n";
+  }
+ void cmd3(const int &, std::istream &, std::ostream & out)
+  {
+    out << "CMD3 dispatched\n";
+  }
+}
 int main()
 {
-  std::cout << "wazup goth programmer nation\n";
+  int context = 0;
+  std::map< std::string, std::function < void(std::istream &, std::ostream &) > > cmds;
+  {
+    using namespace std::placeholders;
+    cmds["CMD1"] = std::bind(goth::cmd1, context, _1, _2);
+    cmds["CMD2"] = std::bind(goth::cmd2, context, _1, _2);
+    cmds["CMD3"] = std::bind(goth::cmd3, context, _1, _2);
+  }
+  std::string cmd;
+  while (std::cin >> cmd)
+  {
+    try
+    {
+      cmds.at(cmd)(std::cin, std::cout);
+    }
+    catch (const std::out_of_range &)
+    {
+      std::cerr << "<INVALID COMMAND>\n";
+    }
+  }
 }
